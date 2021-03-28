849962d038e578ebff1b81c1fc9f1344913b15d8
Wucidici <yxangi@163.com>
2021-03-28 22:13:53 +0800

    提交所有

---
+++ src/components/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="about">
+        <v-header />
+        <v-sidebar />
+        <div class="content-box" :class="{ 'content-collapse': collapse }">
+            <v-tags></v-tags>
+            <div class="content">
+                <router-view v-slot="{ Component }">
+                    <transition name="move" mode="out-in">
+                        <keep-alive :include="tagsList">
+                            <component :is="Component" />
+                        </keep-alive>
+                    </transition>
+                </router-view>
+                <!-- <el-backtop target=".content"></el-backtop> -->
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import vHeader from '../components/main/Header'
+import vSidebar from '../components/main/SideBar'
+import vTags from '../components/main/Tags'
+export default {
+  components: {
+    vHeader,
+    vSidebar,
+    vTags
+  },
+  computed: {
+    tagsList () {
+
+    },
+    collapse () {
+
+    }
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/main/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HomePage',
+      component: () => import('@/components/HomePage.vue')
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
